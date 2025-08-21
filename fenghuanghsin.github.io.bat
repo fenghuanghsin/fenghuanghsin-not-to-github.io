7c10a95ce7ebf90168d3381e20b4e65fea027283
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-22 00:13:16 +0800

    20250821 v2

--- fenghuanghsin.github.io.bat
+++ fenghuanghsin.github.io.bat
@@ -1,7 +1,11 @@
 cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack\public"
 git init
 git add .
+<<<<<<< HEAD
 git commit -m "20250821 v3"
+=======
+git commit -m "20250821 v5"
+>>>>>>> 462b621 (20250821 v2)
 git remote add origin https://github.com/fenghuanghsin/fenghuanghsin.github.io.git
 git checkout -b main
 git push -u origin main --force
