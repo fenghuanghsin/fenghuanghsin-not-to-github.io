775116c12c47a6edaa6cacf8c076da639535771c
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-21 23:40:03 +0800

    20250821 v2

---
+++ fenghuanghsin-not-to-github.io.bat
@@ -0,0 +1,12 @@
+cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack"
+git init
+git add .
+git commit -m "20250821 v2"
+git remote add origin https://github.com/fenghuanghsin/fenghuanghsin-not-to-github.io.git
+git checkout -b main
+git pull origin main --rebase
+git checkout --ours $(git diff --name-only --diff-filter=U)
+git add $(git diff --name-only --diff-filter=U)
+git rebase --continue
+git push -u origin main
+pause
---
+++ fenghuanghsin.github.io.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack\public"
+git init
+git add .
+git commit -m "20250821 v3"
+git remote add origin https://github.com/fenghuanghsin/fenghuanghsin.github.io.git
+git checkout -b main
+git push -u origin main --force
+pause
