e60b7d1947fcc52a6ca34e2bd7c86ca1c7e2932a
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-22 21:49:40 +0800

    20250822 v6

--- fenghuanghsin-not-to-github.io.bat
+++ fenghuanghsin-not-to-github.io.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack"
 git init
 git add .
-git commit -m "20250822 v3"
+git commit -m "20250822 v6"
 git remote add origin https://github.com/fenghuanghsin/fenghuanghsin-not-to-github.io.git
 git checkout -b main
 git pull origin main --rebase
