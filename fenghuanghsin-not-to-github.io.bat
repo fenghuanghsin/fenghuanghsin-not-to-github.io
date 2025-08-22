bf714628d100969336e9b6281462238920f75a98
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-22 21:29:40 +0800

    20250822 v6

--- fenghuanghsin-not-to-github.io.bat
+++ fenghuanghsin-not-to-github.io.bat
@@ -1,13 +1,12 @@
 cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack"
 git init
 git add .
-git commit -m "20250822 v1"
+git commit -m "20250822 v3"
 git remote add origin https://github.com/fenghuanghsin/fenghuanghsin-not-to-github.io.git
 git checkout -b main
 git pull origin main --rebase
-git checkout --ours $(git diff --name-only --diff-filter=U)
-git add $(git diff --name-only --diff-filter=U)
-git add -u
 git rebase --continue
+git status
+pause
 git push -u origin main
 pause
--- fenghuanghsin.github.io.bat
+++ fenghuanghsin.github.io.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack\public"
 git init
 git add .
-git commit -m "20250822 v1"
+git commit -m "20250822 v6"
 git remote add origin https://github.com/fenghuanghsin/fenghuanghsin.github.io.git
 git checkout -b main
 git push -u origin main --force
