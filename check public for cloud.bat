f10de422c10d4d6df0ff38313512b42d1032ed99
goldshoot0720 <goldshoot0720@gmail.com>
2025-12-05 03:45:49 +0800

    20251205 v1

--- check public for cloud.bat
+++ check public for cloud.bat
@@ -1,3 +1,3 @@
-cd "C:\Users\chbon\Documents\fenghuanghsinhugomultithemes\deploy\fenghuanghsin.github.io\stack"
+cd "C:\Users\chbon\Downloads\fenghuanghsin-not-to-github.io-main"
 hugo --gc --minify
 pause
